0f5f22cc75773b75eadd4a10d28e286736b0a592
Lập trình <hieunguyen120318@gmail.com>
2021-02-26 19:33:29 +0700

    JavaOPP1.2

--- JavaOOP1/src/App.java
+++ JavaOOP1/src/App.java
@@ -1,16 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Student student1 = new Student(1, "Nguyen Van A", 7.5f, 9.0f);
-        Student student2 = new Student(2, "Nguyen Van B", 9.5f, 9.5f);
-        Student student3 = new Student(3, "Nguyen Van C", 5.5f, 6.5f);
-        Student student4 = new Student();
-        student4.id = 4;
-        student4.fullName = "Nguyen Van D";
-        student4.theoryPoint = 9.1f;
-        student4.practicePoint = 9.2f;
-        student1.averagePoint();
-        student2.averagePoint();
-        student3.averagePoint();
-        student4.averagePoint();
+        
     }
 }
--- JavaOOP1/src/Student.java
+++ JavaOOP1/src/Student.java
@@ -5,7 +5,6 @@ public class Student {
     float practicePoint;
     // constructor mac dinh:
     public Student(){
-        System.out.println("Constructor mac dinh");
     }
     // constructor co tham so:
     public Student(int id, String fullName, float theoryPoint, float practicePoint){
@@ -16,7 +15,33 @@ public class Student {
     }
 
     // method tinh diem trung binh:
-    public void averagePoint(){
-        System.out.println("Diem trung binh: " + (theoryPoint + practicePoint)/2);
+    public float averagePoint(){
+        return (theoryPoint + practicePoint)/2;
+    }
+    public void display(){
+        System.out.printf("%3d \t %s \t %10.2f \t %16.2f \t %16.2f \n",id,fullName,theoryPoint,practicePoint, averagePoint());
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Mã SV \t   Họ và tên \t Điểm lý thuyết \t Điểm thực hành \t Điểm trung bình");
+        Student sv1 = new Student();
+        sv1.id = 1;
+        sv1.fullName = "Nguyen Van A";
+        sv1.theoryPoint = 6.5f;
+        sv1.practicePoint = 9.5f;
+        Student sv2 = new Student();
+        sv2.id = 2;
+        sv2.fullName = "Nguyen Van B";
+        sv2.theoryPoint = 7.5f;
+        sv2.practicePoint = 9.3f;
+        Student sv3 = new Student();
+        sv3.id = 3;
+        sv3.fullName = "Nguyen Van C";
+        sv3.theoryPoint = 5.5f;
+        sv3.practicePoint = 6.5f;
+        Student sv4 = new Student(4, "Nguyen Van D", 9.1f, 9.2f);
+        sv1.display();
+        sv2.display();
+        sv3.display();
+        sv4.display();
     }
 }
